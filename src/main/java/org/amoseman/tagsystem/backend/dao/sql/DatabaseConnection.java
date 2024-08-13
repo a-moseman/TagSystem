a857bddc1e01a75969f4c6b2cf0e4323ef18a0a8
a-moseman <drewmoseman@gmail.com>
2024-08-13 08:02:15 -0400

    Minor modifications and add javadoc

--- src/main/java/org/amoseman/tagsystem/backend/dao/sql/DatabaseConnection.java
+++ src/main/java/org/amoseman/tagsystem/backend/dao/sql/DatabaseConnection.java
@@ -8,16 +8,28 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+/**
+ * Represents a connection to a SQL database.
+ */
 public class DatabaseConnection {
     private final Connection connection;
     private final DSLContext context;
 
-    private DatabaseConnection(Connection connection) {
+    /**
+     * Instantiate a database connection.
+     * @param connection the connection to use.
+     */
+    private DatabaseConnection(final Connection connection) {
         this.connection = connection;
         this.context = DSL.using(connection, SQLDialect.SQLITE);
     }
 
-    public static DatabaseConnection generate(String url) {
+    /**
+     * Generate a database connection using the provided URL.
+     * @param url the URL of the connection.
+     * @return the database connection.
+     */
+    public static DatabaseConnection generate(final String url) {
         try {
              Connection connection  = DriverManager.getConnection(url);
              return new DatabaseConnection(connection);
@@ -27,10 +39,19 @@ public class DatabaseConnection {
         }
     }
 
+    /**
+     * Get the connection to the database.
+     * @return the connection.
+     */
     public Connection getConnection() {
         return connection;
     }
 
+    /**
+     * Get the DSLContext for the database connection.
+     * To be used by DAOs for SQL queries.
+     * @return the context.
+     */
     public DSLContext context() {
         return context;
     }
