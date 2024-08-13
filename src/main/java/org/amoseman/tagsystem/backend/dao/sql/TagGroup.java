40c3e6dd0c13b8303aa510145f53c8ae58b52544
a-moseman <drewmoseman@gmail.com>
2024-08-13 07:59:33 -0400

    Minor modifications and add javadoc

--- src/main/java/org/amoseman/tagsystem/backend/dao/sql/TagGroup.java
+++ src/main/java/org/amoseman/tagsystem/backend/dao/sql/TagGroup.java
@@ -6,17 +6,33 @@ import org.jooq.impl.DSL;
 
 import static org.jooq.impl.DSL.field;
 
+/**
+ * Represents a group of tags.
+ * They are all grouped by a parent tag with they inherit.
+ */
 public class TagGroup {
     private ImmutableList<String> tags;
 
-    public TagGroup(ImmutableList<String> tags) {
+    /**
+     * Instantiate a new tag group.
+     * @param tags the tags in the group.
+     */
+    public TagGroup(final ImmutableList<String> tags) {
         this.tags = tags;
     }
 
+    /**
+     * Get the tags within the group.
+     * @return the tags within the group.
+     */
     public ImmutableList<String> getTags() {
         return tags;
     }
 
+    /**
+     * Convert the tag group into a jooq condition for use in entity retrieval.
+     * @return the corresponding condition.
+     */
     public Condition asCondition() {
         Condition condition = DSL.falseCondition();
         for (String tag : tags) {
