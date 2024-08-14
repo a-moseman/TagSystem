26a97d1861ac5ba65c8a09a62d3d897023ff7c7e
a-moseman <drewmoseman@gmail.com>
2024-08-14 09:13:18 -0400

    Add javadoc

--- src/main/java/org/amoseman/tagsystem/backend/pojo/EntityRetrievalRequest.java
+++ src/main/java/org/amoseman/tagsystem/backend/pojo/EntityRetrievalRequest.java
@@ -2,14 +2,25 @@ package org.amoseman.tagsystem.backend.pojo;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+/**
+ * Encapsulates a request for entity retrieval.
+ */
 public class EntityRetrievalRequest {
     private String operator;
     private String[] tags;
 
+    /**
+     * Instantiate an empty entity retrieval request.
+     */
     public EntityRetrievalRequest() {
 
     }
 
+    /**
+     * Instantiate an entity retrieval request.
+     * @param operator the operator to use.
+     * @param tags the tags to use.
+     */
     public EntityRetrievalRequest(String operator, String[] tags) {
         this.operator = operator;
         this.tags = tags;
