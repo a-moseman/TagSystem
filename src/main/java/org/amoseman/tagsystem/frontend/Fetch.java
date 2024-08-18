5ef0969ab194f56b7074f868e2eb5024cde14388
a-moseman <drewmoseman@gmail.com>
2024-08-18 08:36:01 -0400

    Fetch can now make requests without auth

--- src/main/java/org/amoseman/tagsystem/frontend/Fetch.java
+++ src/main/java/org/amoseman/tagsystem/frontend/Fetch.java
@@ -5,6 +5,7 @@ import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
@@ -14,20 +15,37 @@ import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
+/**
+ * Provides HTTP request functionality.
+ */
 public class Fetch {
     private final CloseableHttpClient client;
     private String domain;
     private String auth;
 
+    /**
+     * Instantiate an instance of fetch.
+     */
     public Fetch() {
         this.client = HttpClientBuilder.create().build();
     }
 
+    /**
+     * Set the domain of future requests.
+     * @param domain the domain.
+     * @return this.
+     */
     public Fetch setDomain(String domain) {
         this.domain = domain;
         return this;
     }
 
+    /**
+     * Set the basic credentials of future requests.
+     * @param username the username.
+     * @param password the password.
+     * @return this.
+     */
     public Fetch setAuth(String username, String password) {
         String joined = String.format("%s:%s", username, password);
         String base64 = Base64.getEncoder().encodeToString(joined.getBytes(StandardCharsets.UTF_8));
@@ -39,10 +57,21 @@ public class Fetch {
         return String.format("%s/%s", domain, request);
     }
 
+    /**
+     * Adds the authorization header to the provided request, if any credentials have been provided.
+     * @param base the request.
+     */
+    private void handleAuth(HttpRequestBase base) {
+        if (null == auth) {
+            return;
+        }
+        base.addHeader("Authorization", auth);
+    }
+
 
     public String get(String request, ResponseHandler<String> handler) {
         HttpGet get = new HttpGet(formatRequest(request));
-        get.addHeader("Authorization", auth);
+        handleAuth(get);
         try {
             return client.execute(get, handler);
         }
@@ -53,8 +82,8 @@ public class Fetch {
 
     public String get(String request, String entity, ResponseHandler<String> handler) {
         HttpGetWithEntity get = new HttpGetWithEntity();
+        handleAuth(get);
         get.setURI(URI.create(formatRequest(request)));
-        get.addHeader("Authorization", auth);
         try {
             get.setEntity(new StringEntity(entity));
             return client.execute(get, handler);
@@ -66,7 +95,7 @@ public class Fetch {
 
     public String post(String request, ResponseHandler<String> handler) {
         HttpPost post = new HttpPost(formatRequest(request));
-        post.addHeader("Authorization", auth);
+        handleAuth(post);
         try {
             return client.execute(post, handler);
         }
@@ -77,7 +106,7 @@ public class Fetch {
 
     public String post(String request, String entity, ResponseHandler<String> handler) {
         HttpPost post = new HttpPost(formatRequest(request));
-        post.addHeader("Authorization", auth);
+        handleAuth(post);
         post.addHeader(HttpHeaders.CONTENT_TYPE, "application/json");
         try {
             post.setEntity(new StringEntity(entity));
@@ -90,7 +119,7 @@ public class Fetch {
 
     public String delete(String request, ResponseHandler<String> handler) {
         HttpDelete delete = new HttpDelete(formatRequest(request));
-        delete.addHeader("Authorization", auth);
+        handleAuth(delete);
         try {
             return client.execute(delete, handler);
         }
