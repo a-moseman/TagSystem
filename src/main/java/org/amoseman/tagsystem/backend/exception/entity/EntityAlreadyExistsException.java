139481ff54096cf9032b211ef518074906d4d7a5
a-moseman <drewmoseman@gmail.com>
2024-08-19 19:35:39 -0400

    Improve exception

--- src/main/java/org/amoseman/tagsystem/backend/exception/entity/EntityAlreadyExistsException.java
+++ src/main/java/org/amoseman/tagsystem/backend/exception/entity/EntityAlreadyExistsException.java
@@ -1,4 +1,7 @@
 package org.amoseman.tagsystem.backend.exception.entity;
 
 public class EntityAlreadyExistsException extends Exception {
+    public EntityAlreadyExistsException(String uuid) {
+        super(String.format("Entity %s already exists", uuid));
+    }
 }
