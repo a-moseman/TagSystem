a4131c19b299a34f37b931aa6d8c90f7626ae756
a-moseman <drewmoseman@gmail.com>
2024-08-21 13:24:50 -0400

    Add global logging to UserResource

--- src/main/java/org/amoseman/tagsystem/backend/resources/UserResource.java
+++ src/main/java/org/amoseman/tagsystem/backend/resources/UserResource.java
@@ -13,20 +13,25 @@ import org.amoseman.tagsystem.backend.exception.user.UsernameAlreadyInUseExcepti
 import org.amoseman.tagsystem.backend.service.UserService;
 import org.amoseman.tagsystem.backend.pojo.UserCreationRequest;
 
+import java.util.logging.Logger;
+
 @Path("/users")
 @Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
     private final UserService userService;
     private final UserDAO userDAO;
+    private final Logger logger;
 
     public UserResource(UserService userService, UserDAO userDAO) {
         this.userService = userService;
         this.userDAO = userDAO;
+        this.logger = Logger.getGlobal();
     }
 
     @POST
     public Response request(UserCreationRequest request) {
         userService.request(request);
+        logger.info(String.format("Request for account creation of %s", request.username()));
         return Response.accepted().build();
     }
 
@@ -36,11 +41,14 @@ public class UserResource {
     public Response accept(@Auth User user, @PathParam("username") String username) {
         try {
             if (userService.acceptRequest(username)) {
+                logger.info(String.format("Admin %s accepted account creation request of %s", user.getName(), username));
                 return Response.ok().build();
             }
         } catch (UsernameAlreadyInUseException e) {
+            logger.info(String.format("Admin %s failed to accepted account creation request of %s as the username is already in use", user.getName(), username));
             return Response.status(Response.Status.BAD_REQUEST.getStatusCode(), String.format("the username %s is already in use", username)).build();
         }
+        logger.info(String.format("Admin %s failed to accepted account creation request of %s as there is no corresponding request", user.getName(), username));
         return Response.status(Response.Status.BAD_REQUEST.getStatusCode(), String.format("no request corresponding to the provided username %s", username)).build();
     }
 
@@ -50,9 +58,11 @@ public class UserResource {
     public Response delete(@Auth User user, @PathParam("username") String username) {
         try {
             userDAO.removeUser(username);
+            logger.info(String.format("Admin %s deleted account %s", user.getName(), username));
             return Response.ok().build();
         }
         catch (UserDoesNotExistException e) {
+            logger.info(String.format("Admin %s failed to delete account %s as it does not exist", user.getName(), username));
             return Response.status(Response.Status.BAD_REQUEST.getStatusCode(), String.format("%s does not exist", username)).build();
         }
     }
@@ -60,6 +70,7 @@ public class UserResource {
     @GET
     @RolesAllowed({Roles.ADMIN})
     public Response listRequests(@Auth User user) {
+        logger.info(String.format("Admin %s requested a list of all pending account creation requests", user.getName()));
         return Response.ok(userService.listRequests()).build();
     }
 }
